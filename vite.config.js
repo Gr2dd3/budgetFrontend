2cd4e303a9adbc471d89c8d41a75160c079f4a76
Gr2dd3 <gradde23@gmail.com>
2024-10-10 15:49:15 +0200

    chang index and config

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 export default defineConfig({
   plugins: [svelte()],
   build: {
+    outDir: 'dist', // Lägg till denna rad för att specificera output-mappen
     rollupOptions: {
-      input: './index.html' 
+      input: './index.html'
     }
   }
 });
-
