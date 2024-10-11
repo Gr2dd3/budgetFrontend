35cf6eda191f35ffdddfc617e971a3d2428505e4
Gr2dd3 <gradde23@gmail.com>
2024-10-11 21:52:32 +0200

    new hashed dist

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,15 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 export default defineConfig({
   plugins: [svelte()],
   build: {
-    outDir: 'dist', // Lägg till denna rad för att specificera output-mappen
+    outDir: 'dist', // Se till att denna mapp töms varje gång du bygger
     rollupOptions: {
-      input: './index.html'
+      input: './index.html', // Din huvudingångsfil
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',   // Generera hashade filnamn
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]', 
+      }
     }
-  }
+  },
+  base: './', // Gör länkarna relativa så att de fungerar i alla miljöer
 });
