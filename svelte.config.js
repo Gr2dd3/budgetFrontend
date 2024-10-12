ff4229631cf14a0f22472b7230c935b6d2b367bb
Gr2dd3 <gradde23@gmail.com>
2024-10-12 11:03:58 +0200

    trying with azure adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-static';  // Du kan byta adapter om du vill ha något annat än statisk byggnation
+import azure from 'svelte-adapter-azure-swa';
 
 export default {
   kit: {
     // Om du använder en statisk server eller annan typ av hosting kan du ställa in `adapter-static`
-    adapter: adapter(),
+    adapter: azure(),
     target: '#app',  // Här matchar du id:n i din index.html där appen ska monteras (t.ex. <div id="app"></div>)
     vite: {
       // Vite-specifik konfiguration, om nödvändigt
